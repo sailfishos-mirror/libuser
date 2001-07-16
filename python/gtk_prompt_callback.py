69d326912a344e23aaa0a7ee6876ec10badba7c9
crutcher <none@none>
2001-07-16 15:36:39 -0500

    added callback file

---
+++ python/gtk_prompt_callback.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import gtk
+import gnome.ui
+
+def libuser_gtk_prompt_callback(prompts, title):
+	dialog = gnome.ui.GnomeDialog(b1 = gnome.ui.STOCK_BUTTON_OK)
+
+	table = gtk.GtkTable(rows = len(prompts), cols = 2)
+	dialog.vbox.pack_start(table)
+
+	table.set_row_spacings(4)
+	table.set_col_spacings(4)
+
+	ret_list = []
+	for i in range(len(prompts)):
+		prompt = prompts[i]
+
+		label = gtk.GtkLabel(prompt.prompt)
+		label.set_alignment(1.0, 0.5)
+		table.attach(label, 0, 1, i, i + 1)
+
+		entry = gtk.GtkEntry()
+		entry.set_visibility(prompt.visible)
+		entry.set_text(prompt.default_value)
+		table.attach(entry, 1, 2, i, i + 1)
+
+		ret_list.append((prompt, entry))
+		
+	table.show_all()
+
+	dialog.run_and_close()
+
+	for (prompt, entry) in ret_list:
+		prompt.value = entry.get_text()
+
+regcall(libuser_gtk_prompt_callback, "give me some info")
+
+class fake_prompt:
+	def __init__(self, prompt, visible, default_value, value):
+		self.prompt = prompt
+		self.visible = visible
+		self.default_value = default_value
+		self.value = value
+
+	def __repr__(self):
+		return repr({"prompt" : self.prompt, "visible" : self.visible, "default_value" : self.default_value, "value" : self.value})  
+	
+def harness():
+	list = []
+	for t in [	("User", 1, "crutcher", ""),
+			("Password", 0, "defpasswd", ""),
+			("Real Name", 1, "Crutcher Dunnavant", ""),
+			("Home Dir", 1, "Home Dir", ""),
+			("Shell", 1, "/bin/bash", "")	]:
+		list.append(apply(fake_prompt, t))
+	
+	libuser_gtk_prompt_callback(list)
+
+	print list
+
+harness()
