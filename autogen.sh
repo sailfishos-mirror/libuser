8743745b0b5fd05700f5a0ff1d01f227a13d5701
nalin <none@none>
2002-02-28 19:38:08 -0600

    adjust po/ChangeLog to skip bogosity

--- autogen.sh
+++ autogen.sh
@@ -8,8 +8,10 @@ set -x -e
 CFLAGS="$DEFINES $RPM_OPT_FLAGS -O0 -g3 $CFLAGS" ; export CFLAGS
 libtoolize --force
 cp ChangeLog ChangeLog.old
+cp po/ChangeLog po/ChangeLog.old
 gettextize -f -c --intl
 cat ChangeLog.old > ChangeLog
+cat po/ChangeLog.old > po/ChangeLog
 aclocal # -I ./m4
 automake -a
 autoheader
