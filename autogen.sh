481270a0901e6fe310cf42dc1cb9a76ea3db2ee7
mitr <none@none>
2005-06-04 11:21:48 -0500

    2005-06-04  Miloslav Trmac  <mitr@redhat.com>
    	* autogen.sh: Don't set CFLAGS, we don't run configure any more.
    
    	* configure.in: Use gettext 0.14.3.
    
    	* tests/ldap_test: Really use tests/ldaprc.

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-if test -x /bin/rpm ; then
-	if test x${RPM_OPT_FLAGS} = x ; then
-		RPM_OPT_FLAGS=`rpm --eval '%optflags'`
-	fi
-fi
 set -x -e
-CFLAGS="$DEFINES $RPM_OPT_FLAGS -O0 -g3 $CFLAGS" ; export CFLAGS
 [ -d admin ] || mkdir admin
 gtkdocize --docdir docs/reference
 libtoolize --force
